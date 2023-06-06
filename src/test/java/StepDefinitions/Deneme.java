306ac93928ff15fd9e426dfccc652857d6447e60
Erdem BÜKE <erdembke@gmail.com>
2023-06-06 21:13:01 +0300

    deneme1

--- src/test/java/StepDefinitions/Deneme.java
+++ src/test/java/StepDefinitions/Deneme.java
@@ -5,5 +5,12 @@ import io.cucumber.java.en.Given;
 public class Deneme {
     @Given("User should see main page")
     public void userShouldSeeMainPage() {
+        System.out.println("xdwwqxqw");
+        System.out.println("xwqdxwq");
+        System.out.println("qwxdlkxkwlqşdxkl");
+        System.out.println("xasxaxaxaxx");
+        System.out.println("xqkqlxwklsaklşsa");
+        System.out.println("lllllllllllllllll");
+        System.out.println("aaaaaaaaaaaa");
     }
 }
