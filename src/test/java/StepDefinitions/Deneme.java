9b7a386625dd0bee47e2d2e42dc8aedb0d9e75dc
HaQaN <hakann.tasdelenn@gmail.com>
2023-06-06 21:36:04 +0300

    2. conflict

--- src/test/java/StepDefinitions/Deneme.java
+++ src/test/java/StepDefinitions/Deneme.java
@@ -18,5 +18,12 @@ public class Deneme {
         System.out.println("xqkqlxwklsaklşsa");
         //System.out.println("lllllllllllllllll");
         //System.out.println("aaaaaaaaaaaa");
+
+        System.out.println("hakan conflict");
+        System.out.println("hakan 12conflict");
+        System.out.println("hakan 34conflict");
+        System.out.println("hakan 56conflict");
+        System.out.println("hakan 67conflict");
+
     }
 }
