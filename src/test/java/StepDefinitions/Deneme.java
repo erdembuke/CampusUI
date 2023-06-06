727c1da22cceca77d97d7732c16ee60104595fa2
HaQaN <hakann.tasdelenn@gmail.com>
2023-06-06 21:02:59 +0300

    deneme step def tanımlandı

--- src/test/java/StepDefinitions/Deneme.java
+++ src/test/java/StepDefinitions/Deneme.java
@@ -5,5 +5,16 @@ import io.cucumber.java.en.Given;
 public class Deneme {
     @Given("User should see main page")
     public void userShouldSeeMainPage() {
+
+        System.out.println("hakana code 1");
+        System.out.println("hakana code 2");
+        System.out.println("hakana code 3");
+        System.out.println("hakana code 4");
+
+
     }
+
+
+
+
 }
